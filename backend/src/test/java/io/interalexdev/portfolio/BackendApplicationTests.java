a0359c2411703bd31bd062fb426e3a6141e744dd
InteralexDev <interalex1995@gmail.com>
2026-02-18 11:56:50 +0100

    init spring boot backend project

---
+++ backend/src/test/java/io/interalexdev/portfolio/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.interalexdev.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
